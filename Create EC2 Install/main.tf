7b8f07a3dd813649b31c04a08e91a67fa1bf20e2
sria2000 <44519994+sria2000@users.noreply.github.com>
2026-06-30 12:49:17 +0100

    Delete Create EC2 Install/main.tf

--- Create EC2 Install/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
-}
-
-resource "aws_instance" "example" {
-    ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2023 x86_64 (us-east-1)
-    instance_type = "t3.micro"
-}
